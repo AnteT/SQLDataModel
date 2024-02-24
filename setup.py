9c51055ced2c17911056b4071453746f81c77ed7
ante <antetc@cs.wisc.edu>
2024-02-24 17:55:39 -0600

    merged /src and /future, bumped to v0.1.80 for pypi, modified repo url to reflect rename to /AnteT/SQLDataModel

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
      name='SQLDataModel',  
-     version='0.1.77',
+     version='0.1.80',
      scripts=['src/SQLDataModel/SQLDataModel.py'] ,
      author="Ante Tonkovic-Capin",
      author_email="antetc@icloud.com",
      description="A data model based on in-memory sqlite to fetch, manipulate and push data to and from multiple sources",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/AnteT/sql-model",
+     url="https://github.com/AnteT/SQLDataModel",
      package_dir = {"": "src"},
      packages = setuptools.find_packages(where="src"),
      classifiers=[
