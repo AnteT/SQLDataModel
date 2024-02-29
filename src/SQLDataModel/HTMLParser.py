de36ff1a9e61ade3e616711b57f4c7348df0ac80
ante <antetc@cs.wisc.edu>
2024-02-29 15:54:57 -0600

    caught second issue with html parser, fixed and modified tests

--- src/SQLDataModel/HTMLParser.py
+++ src/SQLDataModel/HTMLParser.py
@@ -56,7 +56,7 @@ class HTMLParser(HTMLParser):
         if not self.found_target or self._ignore_next:
             return
         if self._in_td or self._in_th:
-            self._current_cell.append(data.strip())
+            self._current_cell.append(data)
     
     def handle_endtag(self, tag: str) -> None:
         if self._is_finished:
