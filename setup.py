b0ed8897bf28bf8429c9e5ea82a29c3ec00a03d3
ante <antetc@cs.wisc.edu>
2024-04-08 11:55:27 -0500

    published to pypi as v0.3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
      name='SQLDataModel',  
-     version='0.3.4',
+     version='0.3.5',
      scripts=['src/SQLDataModel/SQLDataModel.py'] ,
      author='Ante Tonkovic-Capin',
      author_email='antetc@icloud.com',
